4f1525c73ad825f4b13bd2e21889647e237a771f
Gopikrishnan S <goldenking2734@gmail.com>
2026-01-21 22:08:41 +0530

    Create 2679-CountDistinctNumbersOnBoard

---
+++ 2679-CountDistinctNumbersOnBoard/2679-CountDistinctNumbersOnBoard.cpp
@@ -0,0 +1,10 @@
+// Last updated: 21/01/2026, 22:08:42
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        if(n==1){
+            return 1;
+        }
+        return n-1;
+    }
+};
