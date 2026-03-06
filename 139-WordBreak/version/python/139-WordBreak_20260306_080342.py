b8f4c3ce34bbe7273dee83ee74d0f263e2b9fc21
Gopikrishnan S <goldenking2734@gmail.com>
2026-03-06 08:03:41 +0530

    Create 139-WordBreak

---
+++ 139-WordBreak/version/python/139-WordBreak_20260306_080342.py
@@ -0,0 +1,10 @@
+# Last updated: 06/03/2026, 08:03:42
+1class Solution(object):
+2    def myPow(self, x, n):
+3        """
+4        :type x: float
+5        :type n: int
+6        :rtype: float
+7        """
+8        return x**n
+9        
