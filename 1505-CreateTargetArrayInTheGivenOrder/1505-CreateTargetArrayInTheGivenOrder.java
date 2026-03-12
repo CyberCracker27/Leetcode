9bf4334c0a892400a997843b00e65e945aeeb957
Gopikrishnan S <goldenking2734@gmail.com>
2026-03-12 22:12:42 +0530

    Create 1505-CreateTargetArrayInTheGivenOrder

---
+++ 1505-CreateTargetArrayInTheGivenOrder/1505-CreateTargetArrayInTheGivenOrder.java
@@ -0,0 +1,14 @@
+// Last updated: 12/03/2026, 22:12:46
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            a.add(index[i],nums[i]);
+        }
+        int[] target=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            target[i]=a.get(i);
+        }
+        return target;
+    }
+}
