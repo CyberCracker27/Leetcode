715d7bc9ffeaf1ac3fc48081959064bbb3d24e0e
Gopikrishnan S <goldenking2734@gmail.com>
2026-04-02 07:34:59 +0530

    Create 3093-SumOfValuesAtIndicesWithKSetBits

---
+++ 3093-SumOfValuesAtIndicesWithKSetBits/3093-SumOfValuesAtIndicesWithKSetBits.java
@@ -0,0 +1,19 @@
+// Last updated: 02/04/2026, 07:35:01
+class Solution {
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            String s=Integer.toBinaryString(i);
+            int count=0;
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(j)=='1'){
+                    count++;
+                }
+            }
+            if(count==k){
+                sum+=nums.get(i);
+            }
+        }
+        return sum;
+    }
+}
