89db5c115a7ad0c4514c5777d14997c8219fa3d3
Gopikrishnan S <goldenking2734@gmail.com>
2026-04-18 07:08:44 +0530

    Create 2566-NumberOfUnequalTripletsInArray

---
+++ 2566-NumberOfUnequalTripletsInArray/2566-NumberOfUnequalTripletsInArray.java
@@ -0,0 +1,16 @@
+// Last updated: 18/04/2026, 07:08:45
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    if(nums[i]!=nums[j] && nums[i]!=nums[k] && nums[j]!=nums[k]){
+                        c++;
+                    }
+                }
+            }
+        }
+        return c;
+    }
+}
