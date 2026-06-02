79106783f8afe3717a76bc107d177db99bab3156
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-02 16:20:43 +0530

    File updated at 6/2/2026, 4:20:43 PM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -1,4 +1,4 @@
-// Last updated: 6/2/2026, 4:10:08 PM
+// Last updated: 6/2/2026, 4:20:43 PM
 1class Solution {
 2    public int[] maxSlidingWindow(int[] a, int k) {
 3        int n = a.length;
@@ -6,15 +6,15 @@
 5		int ri = 0;
 6		Deque<Integer> q = new ArrayDeque<>();
 7		for (int i = 0; i < a.length; i++) {
-8			while (!q.isEmpty() && q.peek() < i - k + 1) {
-9				q.poll();
+8			if(!q.isEmpty() && q.peekFirst() < i - k + 1) {
+9				q.pollFirst();
 10			}
 11			while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
 12				q.pollLast();
 13			}
 14			q.offer(i);
 15			if (i >= k - 1) {
-16				r[ri++] = a[q.peek()];
+16				r[ri++] = a[q.peekFirst()];
 17			}
 18		}
 19		return r;              
