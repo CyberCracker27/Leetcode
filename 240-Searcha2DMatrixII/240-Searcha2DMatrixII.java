63f4ff4198a35aa34c1f87c7dedf55f2f7ebaed3
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-03 20:46:27 +0530

    File updated at 6/3/2026, 8:46:26 PM

--- 240-Searcha2DMatrixII/240-Searcha2DMatrixII.java
+++ 240-Searcha2DMatrixII/240-Searcha2DMatrixII.java
@@ -1,17 +1,30 @@
-// Last updated: 6/3/2026, 8:39:32 PM
+// Last updated: 6/3/2026, 8:46:26 PM
 1class Solution {
-2    public boolean searchMatrix(int[][] matrix, int target) {
-3        int row=0,col=matrix[0].length-1;
-4        while(row<matrix.length&& col>=0){
-5            if(matrix[row][col]==target){
-6                return true;
-7            }else if(matrix[row][col]<target){
-8                row++;
-9            }else{
-10                col--;
-11            }
-12            
-13        }
-14        return false;
-15    }
-16}
+2    public boolean search(int[] nums, int target) {
+3        int left=0,right=nums.length-1;
+4        while(left<=right){
+5            int mid=(left+right)/2;
+6            if(nums[mid]==target){
+7                return true;
+8            }
+9            if(nums[left]==nums[mid]){
+10                left++;
+11                continue;
+12            }
+13            if(nums[left]<=nums[mid]){
+14                if(nums[left]<=target && target<nums[mid]){
+15                    right=mid-1;
+16                }else{
+17                    left=mid+1;
+18                }
+19            }else{
+20                if(nums[mid]<target && target<=nums[right]){
+21                    left=mid+1;
+22                }else{
+23                    right=mid-1;
+24                }
+25            }
+26        }
+27        return false;
+28    }
+29}
