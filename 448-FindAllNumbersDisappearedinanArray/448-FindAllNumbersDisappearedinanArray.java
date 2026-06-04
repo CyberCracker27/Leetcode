d786bc911df1ef47935d977faa976c3d8f9bc37f
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-04 15:55:24 +0530

    File updated at 6/4/2026, 3:55:23 PM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
@@ -1,31 +1,27 @@
-// Last updated: 6/4/2026, 2:56:46 PM
+// Last updated: 6/4/2026, 3:55:23 PM
 1class Solution {
-2    public List<String> buildArray(int[] target, int n) {
-3        List<String> li=new ArrayList<>();
-4        int[] arr=new int[target.length];
-5        int l=target.length;
-6        int j=0;
-7        for(int i=1;i<=n;i++){
-8            if(j<l){
-9                arr[j++]=i;
-10                li.add("Push");
-11            }
-12            boolean a=true;
-13            for(int k=0;k<target.length;k++){
-14                if(target[k]!=arr[k] ){
-15                    if(arr[k]!=0){
-16                        arr[k]=0;
-17                        j--;
-18                        li.add("Pop");
-19                    }
-20                    a=false;
-21                    break;
-22                }  
-23            }
-24            if(a){
-25                break;
-26            }
-27        }
-28        return li;
-29    }
-30}
+2    public int evalRPN(String[] tokens) {
+3        int c=0;
+4        Deque<Integer> s=new ArrayDeque<>();
+5        for(String i:tokens){
+6            if(i.equals("+") || i.equals("-") || i.equals("*") || i.equals("/")){
+7                int b=s.pop();
+8                int a=s.pop();
+9                if(i.equals("+")){
+10                    c=a+b;
+11                }else if(i.equals("-") ){
+12                    c=a-b;
+13                }else if(i.equals("*")){
+14                    c=a*b;
+15                }else{
+16                    c=a/b;
+17                }
+18                s.push(c);
+19
+20            }else{
+21                s.push(Integer.valueOf(i));
+22            }
+23        }
+24        return (tokens.length>1)?c:Integer.valueOf(tokens[0]);
+25    }
+26}
