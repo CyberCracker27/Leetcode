9015e13002f1ccb2355659a954f455e4818de1a7
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-04 14:56:47 +0530

    File updated at 6/4/2026, 2:56:46 PM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.java
@@ -1,16 +1,31 @@
-// Last updated: 6/4/2026, 2:12:21 PM
+// Last updated: 6/4/2026, 2:56:46 PM
 1class Solution {
-2    public List<Integer> findDisappearedNumbers(int[] nums) {
-3        List<Integer> li=new ArrayList<>();
-4        Set<Integer> s=new HashSet<>();
-5        for(int i=0;i<nums.length;i++){
-6            s.add(nums[i]);
-7        }
-8        for(int i=1;i<=nums.length;i++){
-9            if(!s.contains(i)){
-10                li.add(i);
+2    public List<String> buildArray(int[] target, int n) {
+3        List<String> li=new ArrayList<>();
+4        int[] arr=new int[target.length];
+5        int l=target.length;
+6        int j=0;
+7        for(int i=1;i<=n;i++){
+8            if(j<l){
+9                arr[j++]=i;
+10                li.add("Push");
 11            }
-12        }
-13        return li;
-14    }
-15}
+12            boolean a=true;
+13            for(int k=0;k<target.length;k++){
+14                if(target[k]!=arr[k] ){
+15                    if(arr[k]!=0){
+16                        arr[k]=0;
+17                        j--;
+18                        li.add("Pop");
+19                    }
+20                    a=false;
+21                    break;
+22                }  
+23            }
+24            if(a){
+25                break;
+26            }
+27        }
+28        return li;
+29    }
+30}
