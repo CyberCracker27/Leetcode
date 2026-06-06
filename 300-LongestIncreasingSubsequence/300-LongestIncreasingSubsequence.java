1d1d6f422c4953bf44c27e84e0ad5640d75de35c
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-06 16:07:32 +0530

    File updated at 6/6/2026, 4:07:31 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,22 +1,22 @@
-// Last updated: 6/6/2026, 3:05:58 PM
+// Last updated: 6/6/2026, 4:07:31 PM
 1class Solution {
-2    public int minDistance(String word1, String word2) {
-3        int[][] arr=new int[word2.length()+1][word1.length()+1];
-4        for(int i=0;i<=word2.length();i++){
-5            arr[i][0]=i;
-6        }
-7        for(int i=0;i<=word1.length();i++){
-8            arr[0][i]=i;
-9        }
-10        for(int i=1;i<=word2.length();i++){
-11            for(int j=1;j<=word1.length();j++){
-12                if(word2.charAt(i-1)==word1.charAt(j-1)){
-13                    arr[i][j]=arr[i-1][j-1];
-14                }else{
-15                    arr[i][j]=1+Math.min(arr[i-1][j-1],Math.min(arr[i-1][j],arr[i][j-1]));
-16                }
+2    public int minPathSum(int[][] grid) {
+3        int arr[][]=new int[grid.length][grid[0].length];
+4        int prev=0;
+5        for(int i=0;i<grid.length;i++){
+6            arr[i][0]=grid[i][0]+prev;
+7            prev=arr[i][0];
+8        }
+9        prev=0;
+10        for(int i=0;i<grid[0].length;i++){
+11            arr[0][i]=grid[0][i]+prev;
+12            prev=arr[0][i];
+13        }
+14        for(int i=1;i<grid.length;i++){
+15            for(int j=1;j<grid[0].length;j++){
+16                arr[i][j]=grid[i][j]+Math.min(arr[i][j-1],arr[i-1][j]);
 17            }
 18        }
-19        return arr[word2.length()][word1.length()];
+19        return arr[grid.length-1][grid[0].length-1];
 20    }
 21}
