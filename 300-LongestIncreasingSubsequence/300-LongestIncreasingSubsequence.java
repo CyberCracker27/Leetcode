00f0e4e1bbd27321ee3755d7aff53e396dd38522
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-06 15:05:59 +0530

    File updated at 6/6/2026, 3:05:58 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,20 +1,22 @@
-// Last updated: 6/6/2026, 1:56:47 PM
+// Last updated: 6/6/2026, 3:05:58 PM
 1class Solution {
-2    public int lengthOfLIS(int[] nums) {
-3        int n=nums.length;
-4        int arr[]=new int[n];
-5        arr[n-1]=1;
-6        int max=1;
-7        for(int i=n-2;i>=0;i--){
-8            int ans=0;
-9            for(int j=i+1;j<n;j++){
-10                if(nums[i]<nums[j]){
-11                    ans=Math.max(ans,arr[j]);
-12                }
-13            }
-14            arr[i]=ans+1;
-15            max=Math.max(max,arr[i]);
-16        }
-17        return max;   
-18    }
-19}
+2    public int minDistance(String word1, String word2) {
+3        int[][] arr=new int[word2.length()+1][word1.length()+1];
+4        for(int i=0;i<=word2.length();i++){
+5            arr[i][0]=i;
+6        }
+7        for(int i=0;i<=word1.length();i++){
+8            arr[0][i]=i;
+9        }
+10        for(int i=1;i<=word2.length();i++){
+11            for(int j=1;j<=word1.length();j++){
+12                if(word2.charAt(i-1)==word1.charAt(j-1)){
+13                    arr[i][j]=arr[i-1][j-1];
+14                }else{
+15                    arr[i][j]=1+Math.min(arr[i-1][j-1],Math.min(arr[i-1][j],arr[i][j-1]));
+16                }
+17            }
+18        }
+19        return arr[word2.length()][word1.length()];
+20    }
+21}
