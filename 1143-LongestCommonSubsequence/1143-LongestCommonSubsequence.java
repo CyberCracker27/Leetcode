39f5f6f5cf2cef6b09b7f0186e977f05cbedaba7
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-06 12:27:37 +0530

    File updated at 6/6/2026, 12:27:37 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,23 +1,18 @@
-// Last updated: 6/6/2026, 11:19:39 AM
+// Last updated: 6/6/2026, 12:27:37 PM
 1class Solution {
-2    public int longestCommonSubsequence(String text1, String text2) {
-3        int m=text1.length();
-4        int n=text2.length();
-5        int[][] dp=new int[m][n];
-6        for(int[] d:dp) Arrays.fill(d,-1);
-7        return rec(text1,text2,0,0,dp);
-8    }
-9    public int rec(String t1,String t2,int left,int right,int[][] dp){
-10        if(left>=t1.length() || right>=t2.length()){
-11            return 0;
-12        }
-13        if(dp[left][right]!=-1){
-14            return dp[left][right];
-15        }
-16        if(t1.charAt(left)==t2.charAt(right)){
-17            return dp[left][right]=1+rec(t1,t2,left+1,right+1,dp);
-18        }else{
-19            return dp[left][right]=Math.max(rec(t1,t2,left+1,right,dp),rec(t1,t2,left,right+1,dp));
-20        }
-21    }
-22}
+2    public int uniquePaths(int m, int n) {
+3        int[][] arr=new int[m][n];
+4        for(int i=0;i<m;i++){
+5            arr[i][0]=1;
+6        }
+7        for(int i=0;i<n;i++){
+8            arr[0][i]=1;
+9        }
+10        for(int i=1;i<m;i++){
+11            for(int j=1;j<n;j++){
+12                arr[i][j]=arr[i-1][j]+arr[i][j-1];
+13            }
+14        }
+15        return arr[m-1][n-1];
+16    }
+17}
