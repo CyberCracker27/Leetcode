257de3c67d4cfba1bf2e7db4c560dad2a59770db
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-09 09:38:58 +0530

    File updated at 6/9/2026, 9:38:58 AM

--- 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.java
+++ 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.java
@@ -1,13 +1,17 @@
-// Last updated: 6/9/2026, 9:22:16 AM
+// Last updated: 6/9/2026, 9:38:58 AM
 1class Solution {
-2    public boolean canMakeArithmeticProgression(int[] arr) {
-3        Arrays.sort(arr);
-4        int a=arr[0]-arr[1];
-5        for(int i=1;i<arr.length-1;i++){
-6            if(a!=arr[i]-arr[i+1]){
-7                return false;
-8            }
-9        }
-10        return true;
-11    }
-12}
+2    public int pivotInteger(int n) {
+3        int i=1,j=n,c=-1;
+4        int leftc=1,rightc=n;
+5        while(i<j){
+6            if(leftc<=rightc){
+7                i++;
+8                leftc+=i;
+9            }else{
+10                j--;
+11                rightc+=j;
+12            }
+13        }
+14        return (leftc==rightc)?i:-1;
+15    }
+16}
