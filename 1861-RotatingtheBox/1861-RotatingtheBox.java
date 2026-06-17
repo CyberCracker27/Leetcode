84da41a453f080f5d1b219221d7f924c6e13c4dc
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-17 07:23:44 +0530

    File updated at 6/17/2026, 7:23:44 AM

--- 1861-RotatingtheBox/1861-RotatingtheBox.java
+++ 1861-RotatingtheBox/1861-RotatingtheBox.java
@@ -1,18 +1,17 @@
-// Last updated: 6/17/2026, 7:06:32 AM
+// Last updated: 6/17/2026, 7:23:44 AM
 1class Solution {
-2    public int minElement(int[] nums) {
-3        int min=Integer.MAX_VALUE;
-4        for(int i=0;i<nums.length;i++){
-5            int temp=nums[i];
-6            int c=0;
-7            while(temp!=0){
-8                c+=temp%10;
-9                temp/=10;
-10            }
-11            if(c<min){
-12                min=c;
-13            }
-14        }
-15        return min;
-16    }
-17}
+2    public int getCommon(int[] nums1, int[] nums2) {
+3        int i=0,j=0;
+4        while(i<nums1.length && j<nums2.length){
+5            if(nums1[i]==nums2[j]){
+6                return nums1[i];
+7            }
+8            if(nums1[i]<nums2[j]){
+9                i++;
+10            }else{
+11                j++;
+12            }
+13        }
+14        return -1;
+15    }
+16}
