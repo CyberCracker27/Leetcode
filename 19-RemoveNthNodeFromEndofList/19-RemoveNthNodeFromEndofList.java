ac92e6f04c8277f3ab3702975b92d07c253f6243
Gopikrishnan S <goldenking2734@gmail.com>
2026-06-24 07:30:47 +0530

    File updated at 6/24/2026, 7:30:46 AM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
@@ -1,4 +1,4 @@
-// Last updated: 6/24/2026, 7:17:53 AM
+// Last updated: 6/24/2026, 7:30:46 AM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,15 +10,30 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode deleteDuplicates(ListNode head) {
-13        ListNode temp=head;
-14        while(temp!=null && temp.next!=null){
-15            if(temp.val==temp.next.val){
-16                temp.next=temp.next.next;
-17            }else{
-18                temp=temp.next;
-19            }
-20        } 
-21        return head;
-22    }
-23}
+12    public ListNode partition(ListNode head, int x) {
+13        ListNode temp1=head;
+14        List<Integer> li=new ArrayList<>();
+15        while(temp1!=null){
+16            if(temp1.val<x){
+17                li.add(temp1.val);
+18            }
+19            temp1=temp1.next;
+20        }
+21        //Collections.sort(li);
+22        ListNode temp=head;
+23        while(temp!=null){
+24            if(temp.val>=x){
+25                li.add(temp.val);
+26            }
+27            temp=temp.next;
+28        }
+29        ListNode fin=new ListNode();
+30        temp=fin;
+31        for(int y:li){
+32            ListNode t=new ListNode(y);
+33            temp.next=t;
+34            temp=temp.next;
+35        }
+36        return fin.next;
+37    }
+38}
