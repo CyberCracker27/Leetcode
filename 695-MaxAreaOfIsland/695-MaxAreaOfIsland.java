e033989016dea561c9333654b6a4fd72ece736de
Gopikrishnan S <goldenking2734@gmail.com>
2026-07-13 20:51:20 +0530

    Create 695-MaxAreaOfIsland

---
+++ 695-MaxAreaOfIsland/695-MaxAreaOfIsland.java
@@ -0,0 +1,21 @@
+// Last updated: 7/13/2026, 8:51:20 PM
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int c=0,row=grid.length,col=grid[0].length;
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]==1){
+                    c=Math.max(c,travel(grid,i,j));
+
+                }
+            }
+        }
+        return c;
+    }
+    int travel(int[][] grid,int row,int col){
+        if(col>=grid[0].length || col<0 || row<0 || row>=grid.length || grid[row][col]==0) return 0;
+        grid[row][col]=0;
+        return 1+travel(grid,row,col+1)+travel(grid,row,col-1)+travel(grid,row-1,col)+travel(grid,row+1,col);
+    }
+}
