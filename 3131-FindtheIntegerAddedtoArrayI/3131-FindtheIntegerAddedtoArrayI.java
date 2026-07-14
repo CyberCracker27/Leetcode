716f8159f1f7ccc639381be56c95c3afbab77c8d
Gopikrishnan S <goldenking2734@gmail.com>
2026-07-14 06:52:54 +0530

    File updated at 7/14/2026, 6:52:53 AM

--- 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.java
+++ 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.java
@@ -1,12 +1,19 @@
-// Last updated: 7/14/2026, 6:46:43 AM
+// Last updated: 7/14/2026, 6:52:53 AM
 1class Solution {
-2    public int addedInteger(int[] nums1, int[] nums2) {
-3        int a=0,b=0;
-4        for(int i=0;i<nums1.length;i++){
-5            a+=nums1[i];
-6            b+=nums2[i];
-7        }
-8        int c=b-a;
-9        return c/nums1.length;
-10    }
-11}
+2    public String mergeAlternately(String word1, String word2) {
+3        StringBuilder s=new StringBuilder();
+4        int a=Math.min(word1.length(),word2.length());
+5        int i=0,j=0;
+6        while(i<a&&j<a){
+7            s.append(word1.charAt(i++));
+8            s.append(word2.charAt(j++));
+9        }
+10        while(i<word1.length()){
+11            s.append(word1.charAt(i++));
+12        }
+13        while(j<word2.length()){
+14            s.append(word2.charAt(j++));
+15        }
+16        return s.toString();
+17    }
+18}
