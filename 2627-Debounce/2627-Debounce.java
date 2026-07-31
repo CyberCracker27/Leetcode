0f5f663d2688979306ef229b659a1cd8bf4d0b92
Gopikrishnan S <goldenking2734@gmail.com>
2026-07-31 06:42:38 +0530

    File updated at 7/31/2026, 6:42:37 AM

--- 2627-Debounce/2627-Debounce.java
+++ 2627-Debounce/2627-Debounce.java
@@ -1,10 +1,10 @@
-// Last updated: 7/30/2026, 6:48:51 AM
+// Last updated: 7/31/2026, 6:42:37 AM
 1class Solution {
-2    public int maxDistinct(String s) {
-3        HashSet<Character> set =new HashSet<>();
-4        for(char ch:s.toCharArray()){
-5            set.add(ch);
+2    public int minPartitions(String n) {
+3        int max=0;
+4        for(int i=0;i<n.length();i++){
+5            max=Math.max(max,n.charAt(i)-'0');
 6        }
-7        return set.size();
+7        return max;
 8    }
 9}
