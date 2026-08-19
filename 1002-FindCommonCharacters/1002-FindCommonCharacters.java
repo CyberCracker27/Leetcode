2eaee6096d78425aff8c9f6ecb09d9927681efef
Gopikrishnan S <goldenking2734@gmail.com>
2026-08-19 06:52:35 +0530

    File updated at 8/19/2026, 6:52:34 AM

--- 1002-FindCommonCharacters/1002-FindCommonCharacters.java
+++ 1002-FindCommonCharacters/1002-FindCommonCharacters.java
@@ -1,13 +1,13 @@
-// Last updated: 8/19/2026, 6:47:00 AM
+// Last updated: 8/19/2026, 6:52:34 AM
 1class Solution {
-2    public int numberOfChild(int n, int k) {
-3        n--;
-4        int ron=k/n;
-5        int rem=k%n;
-6        if(ron%2==0){
-7            return rem;
-8        }else{
-9            return n-rem;
-10        }
+2    public int earliestTime(int[][] tasks) {
+3        int min=Integer.MAX_VALUE;
+4        for(int i=0;i<tasks.length;i++){
+5            int sum=tasks[i][0]+tasks[i][1];
+6            if(sum<min){
+7                min=sum;
+8            }
+9        }
+10        return min;
 11    }
 12}
