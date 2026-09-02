19597eec83c4ba05c56e88e2498c841480609b50
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-02 07:00:57 +0530

    File updated at 9/2/2026, 7:00:57 AM

--- 2221-FindTriangularSumofanArray/2221-FindTriangularSumofanArray.java
+++ 2221-FindTriangularSumofanArray/2221-FindTriangularSumofanArray.java
@@ -1,23 +1,11 @@
-// Last updated: 9/2/2026, 6:58:07 AM
+// Last updated: 9/2/2026, 7:00:57 AM
 1class Solution {
 2    public int triangularSum(int[] nums) {
-3        List<Integer> li=new ArrayList<>();
-4        List<Integer> li1=new ArrayList<>();
-5        for(int i=0;i<nums.length;i++){
-6            li.add(nums[i]);
-7            li1.add(nums[i]);
-8        }
-9        while(li1.size()!=1){
-10            li1=new ArrayList<>();
-11            for(int i=0;i<li.size()-1;i++){
-12                int a=li.get(i)+li.get(i+1);
-13                li1.add(a%10);
-14            }
-15            li=new ArrayList<>();
-16            for(int i=0;i<li1.size();i++){
-17                li.add(li1.get(i));
-18            }
-19        }
-20        return li1.get(0);
-21    }
-22}
+3        for(int n=nums.length;n>1;n--){
+4            for(int i=0;i<n-1;i++){
+5                nums[i]=(nums[i]+nums[i+1])%10;
+6            }
+7        }
+8        return nums[0];
+9    }
+10}
