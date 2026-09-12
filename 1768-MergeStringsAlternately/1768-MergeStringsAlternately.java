0dc90ed7a7430d4f03a945a3cf93b865b5630c48
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:30:01 +0530

    Create 1768-MergeStringsAlternately

---
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -0,0 +1,19 @@
+// Last updated: 9/12/2026, 7:30:01 AM
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder s=new StringBuilder();
+        int a=Math.min(word1.length(),word2.length());
+        int i=0,j=0;
+        while(i<a&&j<a){
+            s.append(word1.charAt(i++));
+            s.append(word2.charAt(j++));
+        }
+        while(i<word1.length()){
+            s.append(word1.charAt(i++));
+        }
+        while(j<word2.length()){
+            s.append(word2.charAt(j++));
+        }
+        return s.toString();
+    }
+}
