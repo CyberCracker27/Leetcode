4c2d974ee94afa307ac19db945562b72fa2da12f
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:27:00 +0530

    Create 2009-MinimumNumberOfOperationsToMakeArrayContinuous

---
+++ 2009-MinimumNumberOfOperationsToMakeArrayContinuous/2009-MinimumNumberOfOperationsToMakeArrayContinuous.java
@@ -0,0 +1,24 @@
+// Last updated: 9/12/2026, 7:26:59 AM
+class Solution {
+    public int minOperations(int[] nums) {
+        int length = nums.length;
+        int minOperations = length;
+        Set<Integer> uniqueNums = new HashSet<>();
+        for (int num : nums) {
+            uniqueNums.add(num);
+        }
+        Integer[] sortedUniqueNums = uniqueNums.toArray(new Integer[uniqueNums.size()]);
+        Arrays.sort(sortedUniqueNums);
+        int right = 0;
+
+        for (int left = 0; left < sortedUniqueNums.length; left++) {
+            while (right < sortedUniqueNums.length && sortedUniqueNums[right] < sortedUniqueNums[left] + length) {
+                right++;
+            }
+
+            minOperations = Math.min(minOperations, length - (right - left));
+        }
+
+        return minOperations;        
+    }
+}
