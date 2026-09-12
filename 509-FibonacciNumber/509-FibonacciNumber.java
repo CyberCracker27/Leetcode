e8b1f30a99a3d4039bf4e7259151fb55e8419c12
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:38:43 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 9/12/2026, 7:38:43 AM
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1; 
+        }else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+}
