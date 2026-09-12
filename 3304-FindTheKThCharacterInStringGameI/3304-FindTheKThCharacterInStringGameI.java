48086f481ba2dc11b8e924aa576c2970d0930293
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:20:39 +0530

    Create 3304-FindTheKThCharacterInStringGameI

---
+++ 3304-FindTheKThCharacterInStringGameI/3304-FindTheKThCharacterInStringGameI.java
@@ -0,0 +1,13 @@
+// Last updated: 9/12/2026, 7:20:39 AM
+class Solution {
+    public char kthCharacter(int k) {
+        StringBuilder sb = new StringBuilder("a");
+        while (sb.length() < k) {
+            int size = sb.length();
+            for (int i = 0; i < size; i++) {
+                sb.append((char) ('a' + ((sb.charAt(i) - 'a') + 1) % 26));
+            }
+        }
+        return sb.charAt(k - 1);
+    }
+}
