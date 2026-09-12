be89afdf338c403232824e6b395efed9e5b17cc7
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:21:06 +0530

    Create 3162-FindTheNumberOfGoodPairsI

---
+++ 3162-FindTheNumberOfGoodPairsI/3162-FindTheNumberOfGoodPairsI.java
@@ -0,0 +1,14 @@
+// Last updated: 9/12/2026, 7:21:06 AM
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int c=0;
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]%(nums2[j]*k)==0){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
