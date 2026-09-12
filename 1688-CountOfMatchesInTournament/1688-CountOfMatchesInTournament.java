1a35c10f9cac37761eb0510ad1530e2739fdd962
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:30:56 +0530

    Create 1688-CountOfMatchesInTournament

---
+++ 1688-CountOfMatchesInTournament/1688-CountOfMatchesInTournament.java
@@ -0,0 +1,16 @@
+// Last updated: 9/12/2026, 7:30:55 AM
+class Solution {
+    public int numberOfMatches(int n) {
+        int c=0;
+        while(n>1){
+            if(n%2==0){
+                c=c+(n/2);
+                n=n/2;
+            }else{
+                c=c+((n-1)/2);
+                n=((n-1)/2)+1;
+            }
+        }
+        return c;
+    }
+}
