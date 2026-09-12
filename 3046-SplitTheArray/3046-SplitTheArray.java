4713f32c13aeb3dd82976a80574a7ccd7e88fddb
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:21:39 +0530

    Create 3046-SplitTheArray

---
+++ 3046-SplitTheArray/3046-SplitTheArray.java
@@ -0,0 +1,15 @@
+// Last updated: 9/12/2026, 7:21:39 AM
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int x:nums){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+        for(int x:map.keySet()){
+            if(map.get(x)>2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
