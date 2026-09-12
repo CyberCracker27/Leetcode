afeeae50543b821a84c98464a9c0cccd887e703d
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:24:34 +0530

    Create 2404-MostFrequentEvenElement

---
+++ 2404-MostFrequentEvenElement/2404-MostFrequentEvenElement.java
@@ -0,0 +1,21 @@
+// Last updated: 9/12/2026, 7:24:34 AM
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        HashMap<Integer,Integer> a=new HashMap<>();
+        int max=0;
+        int b=-1;
+        Arrays.sort(nums);
+        for(int i:nums){
+            a.put(i,a.getOrDefault(i,0)+1);
+        }
+        for(Integer i:a.keySet()){
+            if(i%2==0){
+                if(a.get(i)>max || (a.get(i)==max && i<b)){
+                    max=a.get(i);
+                    b=i;
+                }
+            }
+        }
+        return b;
+    }
+}
