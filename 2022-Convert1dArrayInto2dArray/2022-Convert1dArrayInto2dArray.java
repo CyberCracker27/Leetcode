6b560e89b9b7f3e9a0fe30b992d69fa09ad68579
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:26:39 +0530

    Create 2022-Convert1dArrayInto2dArray

---
+++ 2022-Convert1dArrayInto2dArray/2022-Convert1dArrayInto2dArray.java
@@ -0,0 +1,17 @@
+// Last updated: 9/12/2026, 7:26:38 AM
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int a[][]=new int[m][n];
+        // int b=original.length();
+        if(original.length!=m*n){
+            return new int[0][0];
+        }
+        int c=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]=original[c++];
+            }
+        }
+        return a;
+    }
+}
