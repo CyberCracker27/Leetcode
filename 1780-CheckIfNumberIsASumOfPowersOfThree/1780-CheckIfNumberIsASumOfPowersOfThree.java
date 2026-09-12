f776f3b7390f4220e38ecbb63b64c58442a60138
Gopikrishnan S <goldenking2734@gmail.com>
2026-09-12 07:30:05 +0530

    Create 1780-CheckIfNumberIsASumOfPowersOfThree

---
+++ 1780-CheckIfNumberIsASumOfPowersOfThree/1780-CheckIfNumberIsASumOfPowersOfThree.java
@@ -0,0 +1,12 @@
+// Last updated: 9/12/2026, 7:30:05 AM
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        while(n>0){
+            if(n%3==2){
+                return false;
+            }
+            n/=3;
+        }
+        return true;
+    }
+}
